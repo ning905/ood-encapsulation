99cd09fab5935a24fb6eafc1f4470c9ea8301ddb
Ning Ma <ningma905@gmail.com>
2022-06-15 16:47:11 +0100

    exercise 4

--- spec/SecretDiary.spec.js
+++ spec/SecretDiary.spec.js
@@ -1,30 +1,28 @@
 const SecretDiary = require("../src/SecretDiary");
-const UI = require("../src/UI");
+const Entry = require("../src/Entry");
 
 describe("Secret Diary", () => {
   let secretDiary;
-  let interface;
 
   beforeEach(() => {
     secretDiary = new SecretDiary();
-    interface = new UI();
   });
 
   it("sets the initial state as locked ", () => {
     //set up
     const expected = "locked";
     //execute
-    const result = interface.getState(secretDiary);
+    const result = secretDiary.lock.getStatus();
     //verify
     expect(expected).toEqual(result);
   });
 
   it("can set the state as unlocked", () => {
     //set up
+    secretDiary.lock.unlock();
     const expected = "unlocked";
     //execute
-    secretDiary.unlock();
-    const result = interface.getState(secretDiary);
+    const result = secretDiary.lock.getStatus();
     //verify
     expect(expected).toEqual(result);
     //verify
@@ -32,43 +30,126 @@ describe("Secret Diary", () => {
 
   it("can set the state back to locked", () => {
     //set up
+    secretDiary.lock.unlock();
+    secretDiary.lock.lock();
     const expected = "locked";
     //execute
-    secretDiary.unlock();
-    secretDiary.lock();
-    const result = interface.getState(secretDiary);
+    const result = secretDiary.lock.getStatus();
     //verify
     expect(expected).toEqual(result);
   });
 
   it("cannot add new entries when the state is locked", () => {
     //set up
+    const newEntry = new Entry("Create a secret diary");
     const expected = "Error. The diary is locked.";
     //execute
-    const result = interface.addEntry(secretDiary, "new diary");
+    const result = secretDiary.addEntry(newEntry);
     //verify
     expect(expected).toEqual(result);
   });
 
   it("can add new entries when the state is unlocked", () => {
     //set up
-    const expected = ["new diary"];
+    const newEntry = new Entry("2022-06-15", "Created a secret diary");
+    secretDiary.lock.unlock();
+    secretDiary.addEntry(newEntry);
+    const expected = [
+      { date: "2022-06-15", content: "Created a secret diary" },
+    ];
     //execute
-    secretDiary.unlock();
-    secretDiary.addEntry("new diary");
-    const result = interface.getEntries(secretDiary);
+    const result = secretDiary.getEntries();
     //verify
-    expect(expected).toEqual(result);
+    expect(expected.date).toEqual(result.date);
+    expect(expected.content).toEqual(result.content);
   });
 
   it("cannot print entries when the state is locked", () => {
     //set up
+    const newEntry = new Entry("2022-06-15", "Created a secret diary");
+    secretDiary.lock.unlock();
+    secretDiary.addEntry(newEntry);
+    secretDiary.lock.lock();
     const expected = "Error. The diary is locked.";
     //execute
-    secretDiary.unlock();
-    secretDiary.addEntry("new diary");
-    secretDiary.lock();
-    const result = interface.getEntries(secretDiary);
+    const result = secretDiary.getEntries();
+    //verify
+    expect(expected).toEqual(result);
+  });
+
+  it("can find entries by date when the state is unlocked", () => {
+    //set up
+    const newEntry1 = new Entry(
+      "2022-06-14",
+      "Wrote code for managing cohorts"
+    );
+    const newEntry2 = new Entry(
+      "2022-06-15",
+      "Created a test code secret diary"
+    );
+    const newEntry3 = new Entry("2022-06-15", "Created a secret diary");
+    secretDiary.lock.unlock();
+    secretDiary.addEntry(newEntry1);
+    secretDiary.addEntry(newEntry2);
+    secretDiary.addEntry(newEntry3);
+    const date = "2022-06-15";
+
+    const expected = [
+      { date: "2022-06-15", content: "Created a test code secret diary" },
+      { date: "2022-06-15", content: "Created a secret diary" },
+    ];
+    //execute
+    const result = secretDiary.getEntriesByDate(date);
+    //verify
+    expect(expected.date).toEqual(result.date);
+    expect(expected.content).toEqual(result.content);
+  });
+
+  it("cannot find entries by date when the state is locked", () => {
+    //set up
+    const newEntry1 = new Entry(
+      "2022-06-14",
+      "Wrote code for managing cohorts"
+    );
+    const newEntry2 = new Entry(
+      "2022-06-15",
+      "Created a test code secret diary"
+    );
+    const newEntry3 = new Entry("2022-06-15", "Created a secret diary");
+    secretDiary.lock.unlock();
+    secretDiary.addEntry(newEntry1);
+    secretDiary.addEntry(newEntry2);
+    secretDiary.addEntry(newEntry3);
+    const date = "2022-06-15";
+    secretDiary.lock.lock();
+
+    const expected = "Error. The diary is locked.";
+    //execute
+    const result = secretDiary.getEntriesByDate(date);
+    //verify
+    expect(expected).toEqual(result);
+  });
+
+  it("can return error message if no entries is found on the given date", () => {
+    //set up
+    const newEntry1 = new Entry(
+      "2022-06-14",
+      "Wrote code for managing cohorts"
+    );
+    const newEntry2 = new Entry(
+      "2022-06-15",
+      "Created a test code secret diary"
+    );
+    const newEntry3 = new Entry("2022-06-15", "Created a secret diary");
+    secretDiary.lock.unlock();
+    secretDiary.addEntry(newEntry1);
+    secretDiary.addEntry(newEntry2);
+    secretDiary.addEntry(newEntry3);
+    const date = "2022-06-13";
+
+    const expected = "No entries on this date.";
+    //execute
+    const result = secretDiary.getEntriesByDate(date);
     //verify
     expect(expected).toEqual(result);
   });
--- src/SecretDiary.js
+++ src/SecretDiary.js
@@ -1,15 +1,54 @@
-class SecretDiary {
+class Entry {
+  constructor(date, content) {
+    this.date = date;
+    this.content = content;
+  }
+
+  getDate() {
+    return this.date;
+  }
+
+  setDate(date) {
+    this.date = date;
+  }
+
+  getContent() {
+    return this.content;
+  }
+
+  setContent(content) {
+    this.content = content;
+  }
+}
+
+class Lock {
   constructor() {
-    this.isLocked = true;
-    this.entries = [];
+    this.status = "locked";
+  }
+
+  getStatus() {
+    return this.status;
   }
 
   lock() {
-    this.isLocked = true;
+    this.status = "locked";
   }
 
   unlock() {
-    this.isLocked = false;
+    this.status = "unlocked";
+  }
+
+  isLocked() {
+    if (this.status === "locked") {
+      return true;
+    }
+    return false;
+  }
+}
+
+class Diary {
+  constructor() {
+    this.entries = [];
   }
 
   getEntries() {
@@ -19,6 +58,65 @@ class SecretDiary {
   addEntry(entry) {
     this.entries.push(entry);
   }
+
+  getEntriesByDate(date) {
+    const found = this.entries.filter((entry) => entry.getDate() === date);
+    if (found.length > 0) {
+      return found;
+    }
+    return "No entries on this date.";
+  }
+
+  getEntriesByContent(content) {
+    const found = this.entries.filter(
+      (entry) => entry.getContent() === content
+    );
+    if (found.length > 0) {
+      return found;
+    }
+    return "No such content found.";
+  }
+}
+
+class SecretDiary {
+  constructor() {
+    this.lock = new Lock();
+    this.diary = new Diary();
+  }
+
+  getEntries() {
+    if (this.lock.isLocked()) {
+      return "Error. The diary is locked.";
+    }
+    return this.diary.getEntries();
+  }
+
+  addEntry(entry) {
+    if (this.lock.isLocked()) {
+      return "Error. The diary is locked.";
+    }
+    this.diary.addEntry(entry);
+  }
+
+  getEntriesByDate(date) {
+    if (this.lock.isLocked()) {
+      return "Error. The diary is locked.";
+    }
+    return this.diary.getEntriesByDate(date);
+  }
+
+  getEntriesByContent(content) {
+    if (this.lock.isLocked()) {
+      return "Error. The diary is locked.";
+    }
+    return this.diary.getEntriesByContent(content);
+  }
 }
 
 module.exports = SecretDiary;
+
+const secretDiary = new SecretDiary();
+const entry = new Entry("2022-06-15", "Created a secret diary");
+console.log(secretDiary.addEntry(entry));
+console.log(secretDiary.getEntries());
+console.log(secretDiary.diary);
